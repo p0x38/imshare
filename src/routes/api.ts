e18871a1977dd42871a6eebb308d21ed6ed42d26
p0x38 <ryuuking0410@gmail.com>
2026-09-11 05:41:23 +0900

    feat: register avatar endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,13 +3,14 @@ import type { FastifyPluginAsync } from "fastify";
 import { healthRoutes } from "./health.js";
 import { userRoutes } from "./users.js";
 import { postRoutes } from "./posts.js";
+import { reactionRoutes } from "./reactions.js";
+import { profileLinkRoutes } from "./profile-links.js";
+import { avatarRoutes } from "./avatars.js";
 import { tagRoutes } from "./tags.js";
 import { categoryRoutes } from "./categories.js";
 import { searchRoutes } from "./search.js";
 import { uploadRoutes } from "./uploads.js";
 import { imageRoutes } from "./images.js";
-import { reactionRoutes } from "./reactions.js";
-import { profileLinkRoutes } from "./profile-links.js";
 
 export const apiRoutes: FastifyPluginAsync = async (fastify) => {
   await fastify.register(healthRoutes);
@@ -17,6 +18,7 @@ export const apiRoutes: FastifyPluginAsync = async (fastify) => {
   await fastify.register(postRoutes);
   await fastify.register(reactionRoutes);
   await fastify.register(profileLinkRoutes);
+  await fastify.register(avatarRoutes);
   await fastify.register(tagRoutes);
   await fastify.register(categoryRoutes);
   await fastify.register(searchRoutes);
