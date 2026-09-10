3a04bc2ca3ee0075dd5015c85d37ed4c373d9b74
p0x38 <ryuuking0410@gmail.com>
2026-09-11 03:37:10 +0900

    chore: make ESLint compatible with TypeScript 7

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,14 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default [
   {
     ignores: ["dist/**", "node_modules/**", "uploads/**", "public/**"],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ["**/*.ts"],
     rules: {
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
     },
   },
-);
+];
