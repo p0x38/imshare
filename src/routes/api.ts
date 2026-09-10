7b7dce1370eb6bbf67ce963c31a180b05a78e04f
p0x38 <ryuuking0410@gmail.com>
2026-09-11 05:40:42 +0900

    feat: register interaction and profile routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -8,11 +8,15 @@ import { categoryRoutes } from "./categories.js";
 import { searchRoutes } from "./search.js";
 import { uploadRoutes } from "./uploads.js";
 import { imageRoutes } from "./images.js";
+import { reactionRoutes } from "./reactions.js";
+import { profileLinkRoutes } from "./profile-links.js";
 
 export const apiRoutes: FastifyPluginAsync = async (fastify) => {
   await fastify.register(healthRoutes);
   await fastify.register(userRoutes);
   await fastify.register(postRoutes);
+  await fastify.register(reactionRoutes);
+  await fastify.register(profileLinkRoutes);
   await fastify.register(tagRoutes);
   await fastify.register(categoryRoutes);
   await fastify.register(searchRoutes);
