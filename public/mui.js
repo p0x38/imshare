0f1793b9426fbabb24da679746dc973565c45795
p0x38 <ryuuking0410@gmail.com>
2026-09-11 22:39:37 +0900

    fix: bundle MUI browser dependencies

--- public/mui.js
+++ public/mui.js
@@ -1,5 +1,5 @@
-import React from "https://esm.sh/react@19.1.1";
-import { createRoot } from "https://esm.sh/react-dom@19.1.1/client";
+import React from "https://esm.sh/react@19.1.1?bundle&target=es2022";
+import { createRoot } from "https://esm.sh/react-dom@19.1.1/client?bundle&target=es2022";
 import {
     AppBar,
     Box,
@@ -15,7 +15,7 @@ import {
     Toolbar,
     Typography,
     createTheme,
-} from "https://esm.sh/@mui/material@9.4.0?deps=react@19.1.1,react-dom@19.1.1";
+} from "https://esm.sh/@mui/material@9.4.0?bundle&target=es2022";
 
 const theme = createTheme({
     palette: {
