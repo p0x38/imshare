4994f6377cf564e0614a38eedc829ae19bff7371
p0x38 <ryuuking0410@gmail.com>
2026-09-12 18:51:35 +0900

    feat: support light and dark app themes

--- src/client/theme.ts
+++ src/client/theme.ts
@@ -1,20 +1,22 @@
 import { createTheme } from "@mui/material/styles";
 
-export const theme = createTheme({
-    palette: {
-        mode: "light",
-    },
-    shape: {
-        borderRadius: 3,
-    },
-    typography: {
-        fontFamily: "Roboto, system-ui, sans-serif",
-    },
-    components: {
-        MuiButton: {
-            defaultProps: {
-                disableElevation: true,
+export function createAppTheme(mode: "light" | "dark") {
+    return createTheme({
+        palette: {
+            mode,
+        },
+        shape: {
+            borderRadius: 3,
+        },
+        typography: {
+            fontFamily: "Roboto, system-ui, sans-serif",
+        },
+        components: {
+            MuiButton: {
+                defaultProps: {
+                    disableElevation: true,
+                },
             },
         },
-    },
-});
+    });
+}
