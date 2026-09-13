90f3a40677ae13910267351ed611e75f2d107700
p0x38 <ryuuking0410@gmail.com>
2026-09-14 06:19:16 +0900

    test: allow slower application initialization

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
         environment: "node",
         include: ["test/**/*.test.ts"],
         exclude: ["node_modules", "dist", "test/integration.test.ts"],
+        testTimeout: 15_000,
         passWithNoTests: false,
     },
 });
