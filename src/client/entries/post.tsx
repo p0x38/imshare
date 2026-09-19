ba563478d88e4662f1584bd114774c45a0d83e7b
p0x38 <ryuuking0410@gmail.com>
2026-09-20 04:02:55 +0900

    fix(client): resolve user-scoped post permalinks

--- src/client/entries/post.tsx
+++ src/client/entries/post.tsx
@@ -2,12 +2,16 @@ import { createRoot } from "react-dom/client";
 import { App } from "../components/App";
 import { PostPage } from "../components/PostPage";
 
-const postId = decodeURIComponent(location.pathname.replace(/^\/posts\//, "").replace(/\/$/, ""));
+const segments = location.pathname.split("/").filter(Boolean);
+const isPostsPath = segments[0] === "posts";
+const identifier = decodeURIComponent(segments.at(-1) ?? "");
+const postId = isPostsPath ? identifier : undefined;
+const permalinkKey = isPostsPath ? undefined : identifier;
 const root = document.querySelector("#post");
 
 if (root)
     createRoot(root).render(
         <App>
-            <PostPage postId={postId} />
+            <PostPage postId={postId} permalinkKey={permalinkKey} />
         </App>,
     );
