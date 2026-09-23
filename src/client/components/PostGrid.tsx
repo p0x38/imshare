f8942f877d73a6c35654106bbe99a990e8ff5ae8
p0x38 <ryuuking0410@gmail.com>
2026-09-23 15:23:04 +0900

    feat(posts): use custom thumbnails in post grid

--- src/client/components/PostGrid.tsx
+++ src/client/components/PostGrid.tsx
@@ -124,11 +124,11 @@ export function PostGrid({
                             href={`/posts/${encodeURIComponent(post.id)}`}
                             sx={{ display: "block", textAlign: "left", height: "100%" }}
                         >
-                            {post.uploads?.[0] ? (
+                            {post.thumbnailUrl || post.uploads?.[0] ? (
                                 <CardMedia
                                     component="img"
-                                    image={imageUrl(post.uploads[0].url)}
-                                    alt={post.uploads[0].alt || post.title || ""}
+                                    image={imageUrl(post.thumbnailUrl || post.uploads?.[0]?.url || "")}
+                                    alt={post.title || ""}
                                     loading="lazy"
                                     sx={{
                                         aspectRatio: "1 / 1",
